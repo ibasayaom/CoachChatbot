6c6540b2d8112bc9fbac70619890d7ae70478618
Isaac Basaya <113010180+ibasayaom@users.noreply.github.com>
2025-06-20 10:43:34 +0200

    Final push

--- src/Core/Models/ApplicationSettings.cs
+++ src/Core/Models/ApplicationSettings.cs
@@ -11,5 +11,8 @@ namespace Core.Models
         public string  AzureEndpoint { get;set; } = "https://mission-ctrl-resource.openai.azure.com/";
         public string ApiKeyCredential { get; set; } = "CPZv1KFIgVIgqcUjK5aczFFfh1Z92KZHRGAWq0sN0nBFVFZlfdgGJQQJ99BFACHYHv6XJ3w3AAAAACOGpcSB";
         public string ChatClient { get; set; } = "gpt-4o";
+        public string GetAgent { get; set; } = "asst_nbJP1uQwXz7fi3m5J1P0moCn"; // This is the deployment name for the chat client
+
+        public string GetThread { get; set; } = "thread_9Ea9z4flWAW5penedKX8P9lZ"; // This is the model name for the chat client
     }
 }
